05ef69bd0305ded7461108c97c0e929c64b9bb06
Bertolini <victorbertolinidesousa@gmail.com>
2026-02-01 22:35:51 -0300

    fix: Ajustando o commit

--- src/model/entities/SummaryHandler.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class SummaryHandler(ABC):
-    @abstractmethod
-    def show_summary(self) -> None:
-        ...
--- src/model/entities/TransactionType.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-class TransactionType(Enum):
-    INCOME = 'income'
-    EXPENSE = 'expense'
